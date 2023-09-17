0c5bb6ab293f8970615061e2806d1a0288b819cc
Fredrik Jansson <fjansson@abo.fi>
2023-09-17 20:42:46 +0200

    FM: better station name display

--- 8-FM/8-FM.ino
+++ 8-FM/8-FM.ino
@@ -19,44 +19,52 @@ void setup()
     hd44780::fatalError(status); // does not return
   rx.setFrequency(10180);        // frequency(in MHz) * 100
   rx.setAgc(1);                  // enable automatic gain control
-  rx.setExtendedVolumeRange(1);  // set lower volume scale 
+  rx.setExtendedVolumeRange(1);  // set lower volume scale (for earphones)
   rx.setVolume(10);              // set volume (range 0..15, 0 is silent)
-  // rx.setFmDeemphasis(1);      // standard in Europe
+  // rx.setFmDeemphasis(1);      // for the European FM standard
   rx.setRds(true);               // enables RDS
   rx.setRdsMode(1);              
   pinMode(seek_down_pin, INPUT_PULLDOWN);
   pinMode(seek_up_pin, INPUT_PULLDOWN);
 }
 
-char *stationName;
+char str[20];
 int i = 0;
 void loop() 
 {
-  char str[20];
-  // optionally wait for a button press
-  //while(!(digitalRead(gp_seek_down) || digitalRead(gp_seek_up)))
-  //  delay(5);
+  // remove comment signs below to wait for a button press
+  // while(!(digitalRead(seek_down_pin) || digitalRead(seek_up_pin)))
+  //   delay(5);
 
+  if (digitalRead(seek_down_pin) || digitalRead(seek_up_pin))
+  { // clear RDS data when changing station
+    rx.clearRdsBuffer();
+    lcd.clear();
+  }
   if (digitalRead(seek_down_pin))
     rx.seek(0, 0); 
   if (digitalRead(seek_up_pin))
-    rx.seek(0, 1);  
+    rx.seek(0, 1);
+
   delay(30);
 
   if (rx.getRdsReady())
-    stationName = rx.getRdsText0A();
+  {
+    lcd.setCursor(0, 1); // 2nd row
+    lcd.print(rx.getRdsText0A());
+  }
 
-  if (i++ % 10 == 0)
+  if (i++ % 20 == 0) // every 20th time...
   { // fetch information and display on screen
     float f = rx.getRealFrequency() / 100.0;
     int rssi = rx.getRssi();
     int stereo = rx.isStereo();
-    snprintf(str, 18, "%6.2f MHz  %d", f, rssi);
-    lcd.clear();
+    snprintf(str, 18, "%6.2f MHz  %2d", f, rssi);
+    lcd.setCursor(0, 0); // 1st row
     lcd.print(str);
     if (stereo)
       lcd.print(" S");
-    lcd.setCursor(0, 1); 
-    lcd.print(stationName);
+    else
+      lcd.print("  ");
   }
 }
